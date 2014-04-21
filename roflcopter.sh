19da273d36b452140db9096a3104c983d48c0282
Christian Gärtner <christiangaertner.film@googlemail.com>
2014-04-21 18:26:14 +0200

    Removed old spacing variable which I played around with...

--- roflcopter.sh
+++ roflcopter.sh
@@ -15,24 +15,24 @@ RUNNING=true
 while $RUNNING; do
 	trap "clear;RUNNING=false;echo ; echo ;echo The copter crashed... :/; exit 0" 2
 	echo "
-$SPACING ROFL:ROFL:LOL:ROFL:ROF
-$SPACING          __^___
-$SPACING       __/   [] \    
-$SPACING LOL===__        \ 
-$SPACING         \________]
-$SPACING          I   I
-$SPACING         --------/
+ROFL:ROFL:LOL:ROFL:ROF
+         __^___
+      __/   [] \    
+LOL===__        \ 
+        \________]
+         I   I
+        --------/
 "
 sleep 0.4s
 clear
 	echo "
-$SPACING           LOL
-$SPACING          __^___
-$SPACING  L    __/   [] \    
-$SPACING  O====__        \ 
-$SPACING  L      \________]
-$SPACING          I   I
-$SPACING         --------/
+          LOL
+         __^___
+ L    __/   [] \    
+ O====__        \ 
+ L      \________]
+         I   I
+        --------/
 "
 sleep 0.4s
 clear
